cc7ec4eaed512b5aabe4a5fddcec1338bfcb9783
Sean Roelofs <sean.forrester.roelofs@gmail.com>
2021-02-24 18:40:29 -0800

    quad changes to verify in sim

--- commander/src/commander.cpp
+++ commander/src/commander.cpp
@@ -16,6 +16,7 @@
 #include <mavros_msgs/CommandLong.h>
 #include <mavros_msgs/State.h>
 #include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/ActuatorControl.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <trajectory_msgs/MultiDOFJointTrajectory.h>
 #include <string>
@@ -25,10 +26,11 @@
 const float TAKEOFF_ALTITUDE = 1.0;
 const float YAW_PERIOD = 12.0;
 const float DIST_THRESH = 0.15;
-const float Z_OFFSET = 0;//0.06;
-const float X_OFFSET = 0;//-0.10;
 const float MAX_H_SPEED = 0.4;
 const float MAX_V_SPEED = 0.4;
+const float FINAL_GOAL_DIST = 2.0;
+const float Z_OFFSET = 0.06;
+const float X_OFFSET = -0.10;
 
 
 enum State {
@@ -65,8 +67,6 @@ class Commander {
     mavros_msgs::SetMode land_set_mode;
     mavros_msgs::CommandBool arm_cmd;
     mavros_msgs::CommandBool disarm_cmd;
-    mavros_msgs::CommandLong spray_on_cmd;
-    mavros_msgs::CommandLong spray_off_cmd;
 
     ros::NodeHandle nh;
     ros::Rate rate;
@@ -75,7 +75,6 @@ class Commander {
     ros::Publisher mavros_pose_pub;
     ros::ServiceClient arming_client;
     ros::ServiceClient set_mode_client;
-    ros::ServiceClient spray_cmd_client;
     ros::Subscriber goals_sub;
     ros::Subscriber traj_sub;
     ros::Time last_request;
@@ -110,7 +109,6 @@ class Commander {
       mavros_pose_pub = nh.advertise<geometry_msgs::PoseStamped> ("mavros/setpoint_position/local", 10);
       arming_client = nh.serviceClient<mavros_msgs::CommandBool> ("mavros/cmd/arming");
       set_mode_client = nh.serviceClient<mavros_msgs::SetMode> ("mavros/set_mode");
-      spray_cmd_client = nh.serviceClient<mavros_msgs::CommandLong>("cmd/command");
       goals_sub = nh.subscribe("goal_gen/goals", 1, &Commander::goals_cb, this);
       traj_sub = nh.subscribe("waypoints", 1, &Commander::traj_cb, this);
 
@@ -118,29 +116,12 @@ class Commander {
       land_set_mode.request.custom_mode = "AUTO.LAND";
       arm_cmd.request.value = true;
       disarm_cmd.request.value = false;
-      spray_on_cmd.request.command = 187;
-      spray_off_cmd.request.command = 187;
-      spray_on_cmd.request.param1 = -1.0;
-      spray_off_cmd.request.param1 = 1.0;
-      spray_on_cmd.request.param2 = -1.0;
-      spray_off_cmd.request.param2 = 1.0;
-      spray_on_cmd.request.param3 = -1.0;
-      spray_off_cmd.request.param3 = 1.0;
-      spray_on_cmd.request.param4 = -1.0;
-      spray_off_cmd.request.param4 = 1.0;
-      spray_on_cmd.request.param5 = -1.0;
-      spray_off_cmd.request.param5 = 1.0;
-      spray_on_cmd.request.param6 = -1.0;
-      spray_off_cmd.request.param6 = 1.0;
-      spray_on_cmd.request.param7 = 0;
-      spray_off_cmd.request.param7 = 0;
 
-
-      takeoff_goal.pose.position.x = 0 + X_OFFSET;
+      takeoff_goal.pose.position.x = 0;
       takeoff_goal.pose.position.y = 0;
-      takeoff_goal.pose.position.z = TAKEOFF_ALTITUDE + Z_OFFSET;
+      takeoff_goal.pose.position.z = TAKEOFF_ALTITUDE;
 
-      final_goal.pose.position.x = 8 + X_OFFSET;
+      final_goal.pose.position.x = FINAL_GOAL_DIST + X_OFFSET;
       final_goal.pose.position.y = 0;
       final_goal.pose.position.z = TAKEOFF_ALTITUDE + Z_OFFSET;
 
@@ -161,24 +142,30 @@ class Commander {
         curr_goal.pose = traj.poses[1];
       } else if (traj.poses.size() == 1) {
         ROS_INFO("unexpected size 1 traj");
+      } else if (traj.poses.size() == 0 && (ros::Time::now() - explore_start).toSec() > 2.0) {
+        state = LAND;
+        ROS_WARN("No path, landing");
       }
+
       float yaw = 1.3 * sin((ros::Time::now() - explore_start).toSec() * 2 * M_PI / YAW_PERIOD) ;
       tf2::Quaternion q = tf2::Quaternion(0, 0, yaw);  
       curr_goal.pose.orientation.x = q.x();
       curr_goal.pose.orientation.y = q.y();
       curr_goal.pose.orientation.z = q.z();
       curr_goal.pose.orientation.w = q.w();
+      /* // modified
       if (goals.poses.size() > goal_index) {
         curr_request.pose = goals.poses[goal_index];
         curr_request_pub.publish(curr_request);
         state = SPRAY;
       }
+      */
       if (traj.poses.size() == 0 && (ros::Time::now() - explore_start).toSec() > 2.0) {
         state = LAND;
         ROS_WARN("No path, landing");
       }
 
-      if(dist(curr_pose.pose, final_goal.pose) < DIST_THRESH || ros::Time::now() - explore_start > ros::Duration(200.0)) {
+      if(dist(curr_pose.pose, final_goal.pose) < DIST_THRESH && ros::Time::now() - explore_start > ros::Duration(40.0)) { // modified
         state = LAND;
       }
     }
@@ -309,18 +296,6 @@ class Commander {
       
 
     void run() {
-      start_time = ros::Time::now();
-      while (ros::ok()) {
-        if (int((ros::Time::now() - start_time).toSec() / 2.0) % 2 == 0){
-          ROS_INFO("Sprayer On");
-          spray_cmd_client.call(spray_on_cmd);
-        } else {
-          ROS_INFO("Sprayer Off");
-          spray_cmd_client.call(spray_off_cmd);
-        }
-      }
-
-      /*
       prev_state = TAKEOFF;
       state = TAKEOFF;
       start_time = ros::Time::now();
@@ -351,25 +326,49 @@ class Commander {
           wait();
         }
         if (state != prev_state) {
-          ROS_INFO("Switching to state: %s", state_names[state]);//+ std::string(state_names[state]));
+          ROS_INFO("Switching to state: %s", state_names[state]);
         }
 
-        tfBuffer->transform(curr_goal, local_curr_goal, "t265_odom_frame");//, ros::Time(0), "map");
-        //mavros_pose_pub.publish(curr_goal);
-        sim_pub_goal();
+        tfBuffer->transform(curr_goal, local_curr_goal, "t265_odom_frame");
+        mavros_pose_pub.publish(local_curr_goal);
+        //sim_pub_goal();
         prev_state = state;
          
-        ros::spinOnce();
-        rate.sleep();
       } 
-      */
+      ros::spinOnce();
+      rate.sleep();
     } 
 };
 
 
 int main (int argc, char** argv)
 {
-  ros::init (argc, argv, "commander");
-  Commander commander;
-  commander.run();
+    
+
+    ros::init (argc, argv, "commander");
+    Commander commander;
+    commander.run();
+    /*
+    ros::NodeHandle nh;
+    mavros_msgs::ActuatorControl spray_on;
+    mavros_msgs::ActuatorControl spray_off;
+    ros::Rate rate(10.0);
+    ros::Publisher spray_pub = nh.advertise<mavros_msgs::ActuatorControl>("/mavros/actuator_control", 1);
+    spray_on.group_mix = 3;
+    spray_on.controls[5] = 1;
+    spray_off.group_mix = 3;
+    spray_off.controls[5] = -1;
+    ros::Time start_time = ros::Time::now();
+    while (ros::ok()) {
+      if (int((ros::Time::now() - start_time).toSec()) % 2 == 0){
+        ROS_INFO("Sprayer On");
+        spray_pub.publish(spray_on);
+      } else {
+        ROS_INFO("Sprayer Off");
+        spray_pub.publish(spray_off);
+      }
+      ros::spinOnce();
+      rate.sleep();
+    }
+    */
 }
